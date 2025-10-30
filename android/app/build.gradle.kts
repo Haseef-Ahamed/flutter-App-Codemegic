87c6a70708a6ef53b26e2d16bfb1cb137621688e
Haseef-Ahamed <mshaseefat@gmail.com>
2025-10-30 22:48:42 +0530

    codemagic confiduration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,85 +1,47 @@
-import java.io.File
-
-import java.util.*
-
-
-
-val keystoreProperties =
-
-    Properties().apply {
-
-        var file = File("key.properties")
-
-        if (file.exists()) load(file.reader())
-
-    }
-
-
-
-plugins { ... }
-
-
+plugins {
+    id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
 
 android {
+    namespace = "com.example.app"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
 
-    ...
-
-    val appVersionCode = (System.getenv()["NEW_BUILD_NUMBER"] ?: "1")?.toInt()
-
-    defaultConfig {
-
-        ...
-
-        versionCode = appVersionCode
-
-        ...
-
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
-    signingConfigs {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
 
-        create("release") {
-
-            if (System.getenv()["CI"].toBoolean()) { // CI=true is exported by Codemagic
-
-                storeFile = file(System.getenv()["CM_KEYSTORE_PATH"])
-
-                storePassword = System.getenv()["CM_KEYSTORE_PASSWORD"]
-
-                keyAlias = System.getenv()["CM_KEY_ALIAS"]
-
-                keyPassword = System.getenv()["CM_KEY_PASSWORD"]
-
-            } else {
-
-                storeFile = file(keystoreProperties.getProperty("storeFile"))
-
-                storePassword = keystoreProperties.getProperty("storePassword")
-
-                keyAlias = keystoreProperties.getProperty("keyAlias")
-
-                keyPassword = keystoreProperties.getProperty("keyPassword")
-
-            }
-
-        }
-
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "io.codemagic.fluttersample"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
 
     buildTypes {
-
-        getByName("release") {
-
-            isMinifyEnabled = false
-
-            signingConfig = signingConfigs.getByName("release")
-
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
         }
-
     }
-
 }
 
-
-
-dependencies { ... }
+flutter {
+    source = "../.."
+}
